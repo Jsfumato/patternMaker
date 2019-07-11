a8e63675610f6f54552b5168a4d30a2a894abbda
DESKTOP-HOME\jsfumato <jsfumato@naver.com>
2019-07-11 09:54:04 +0900

    ColorWheelControl 추가

---
+++ Assets/Resources/Prefab/Popup_ChangeBrush.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Popup_ChangeBrush : MonoBehaviour {
+
+    public ColorWheelControl colorControl;
+    public delegate void ChangeBrushDelegate(Color color, int size);
+
+    private ChangeBrushDelegate _callback;
+    private Color _selected;
+    private int _size;
+
+    public void Initialzie(ChangeBrushDelegate callback) {
+        _callback = callback;
+    }
+
+    public void OnOK() {
+        var Color = colorControl.Selection;
+
+        if (_callback != null)
+            _callback(_selected, _size);
+
+        //
+        Destroy(gameObject);
+    }
+
+    public void OnCancel() {
+        Destroy(gameObject);
+    }
+}
