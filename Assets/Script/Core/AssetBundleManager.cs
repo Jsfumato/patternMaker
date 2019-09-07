759640731c2050c73879ad0b48b67c31bb341ec2
Hwang JongSung <jsfumato@supercat.co.kr>
2019-09-07 14:08:30 +0900

    assetbundle 추가 중

---
+++ Assets/Script/Core/AssetBundleManager.cs
@@ -0,0 +1,479 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class AssetBundleManager : MonoBehaviour {
+    // 암호화 키
+    public static readonly byte[] KEY = { 0x14, 0x92, 0x73, 0x05 };
+
+    //
+    private static string[] _persistentDataPaths;
+    public static bool IsDirectoryWritable(string path) {
+        try {
+            if (!Directory.Exists(path))
+                return false;
+            string file = Path.Combine(path, Path.GetRandomFileName());
+            using (FileStream fs = File.Create(file, 1)) {
+            }
+            File.Delete(file);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    private static string GetPersistentDataPath(params string[] components) {
+        try {
+            string path = Path.DirectorySeparatorChar + string.Join("" + Path.DirectorySeparatorChar, components);
+            if (!Directory.GetParent(path).Exists)
+                return null;
+            if (!Directory.Exists(path)) {
+                Debug.Log("creating directory: " + path);
+                Directory.CreateDirectory(path);
+            }
+            if (!IsDirectoryWritable(path)) {
+                Debug.LogWarning("persistent data path not writable: " + path);
+                return null;
+            }
+            return path;
+        } catch {
+            return null;
+        }
+    }
+
+    public static string persistentDataPathInternal {
+#if UNITY_ANDROID
+        get
+        {
+            if (Application.isEditor || !Application.isPlaying) return Application.persistentDataPath;
+            string path = null;
+            if (string.IsNullOrEmpty(path)) path = GetPersistentDataPath("storage", "emulated", "0", "Android", "data", Application.identifier, "files");
+            if (string.IsNullOrEmpty(path)) path = GetPersistentDataPath("data", "data", Application.identifier, "files");
+            return path;
+        }
+#else
+        get {
+            return Application.persistentDataPath;
+        }
+#endif
+    }
+
+    public static string persistentDataPathExternal {
+#if UNITY_ANDROID
+        get
+        {
+            if (Application.isEditor || !Application.isPlaying) return null;
+            string path = null;
+            if (string.IsNullOrEmpty(path)) path = GetPersistentDataPath("storage", "sdcard0", "Android", "data", Application.identifier, "files");
+            if (string.IsNullOrEmpty(path)) path = GetPersistentDataPath("storage", "sdcard1", "Android", "data", Application.identifier, "files");
+            if (string.IsNullOrEmpty(path)) path = GetPersistentDataPath("mnt", "sdcard", "Android", "data", Application.identifier, "files");
+            return path;
+        }
+#else
+        get {
+            return null;
+        }
+#endif
+    }
+
+    public static string[] persistentDataPaths {
+        get {
+            if (_persistentDataPaths == null) {
+                List<string> paths = new List<string>();
+                if (!string.IsNullOrEmpty(persistentDataPathInternal))
+                    paths.Add(persistentDataPathInternal);
+                if (!string.IsNullOrEmpty(persistentDataPathExternal))
+                    paths.Add(persistentDataPathExternal);
+                if (!string.IsNullOrEmpty(Application.persistentDataPath) && !paths.Contains(Application.persistentDataPath))
+                    paths.Add(Application.persistentDataPath);
+                _persistentDataPaths = paths.ToArray();
+            }
+            return _persistentDataPaths;
+        }
+    }
+
+    // returns best persistent data path
+    public static string persistentDataPath {
+        get {
+            return persistentDataPaths.Length > 0 ? persistentDataPaths[0] : null;
+        }
+    }
+
+    public delegate void StatusCallback(bool success, string message, long fileSize);
+    public delegate void DownloadCallback(bool success, string message);
+    public delegate void ProgressCallback(float v);
+
+    //
+    public static List<string> assetBundleNames = new List<string>()
+    {
+        "AnimFX",
+        "Clans",
+        "Commons",
+        "FXs",
+        "Image",
+        "Items",
+        "MapObjects",
+        "MapPrefabs",
+        "Maps",
+        "Quests",
+        "Skills",
+        "Units",
+    };
+
+    //
+    private static AssetBundleManager _singleton;
+    public static AssetBundleManager Get() {
+        if (!_singleton) {
+            if (!Application.isPlaying)
+                return null;
+
+            var obj = new GameObject("[AssetBundleManager]");
+            DontDestroyOnLoad(obj);
+
+            _singleton = obj.AddComponent<AssetBundleManager_Custom>();
+        }
+        return _singleton;
+    }
+
+    //
+    [NonSerialized]
+    public AssetBundleInfo commonsAssetBundle;
+
+    public class AssetBundleInfo {
+        public Stream stream;
+        public AssetBundle assetBundle;
+
+        internal AssetBundleInfo(AssetBundle assetBundle, Stream stream) {
+            this.stream = stream;
+            this.assetBundle = assetBundle;
+        }
+
+        public void Destroy() {
+            try {
+                if (assetBundle != null)
+                    assetBundle.Unload(true);
+                assetBundle = null;
+            } catch {
+
+            }
+
+            try {
+                if (stream != null)
+                    stream.Dispose();
+                stream = null;
+            } catch {
+
+            }
+        }
+    }
+
+    //
+    protected readonly List<AssetBundleInfo> assetBundles = new List<AssetBundleInfo>();
+    protected uint _version;
+    public List<ServerGroupInfo> serverGroups = new List<ServerGroupInfo>();
+    public bool loadedAll = false;
+
+    public void UnloadCommonAssetBundle() {
+        if (commonsAssetBundle != null) {
+            commonsAssetBundle.Destroy();
+            assetBundles.Remove(commonsAssetBundle);
+            commonsAssetBundle = null;
+        }
+    }
+
+    public virtual void Awake() {
+
+        //
+        var cache = Caching.currentCacheForWriting;
+        cache.expirationDelay = 12960000;
+        cache.maximumAvailableStorageSpace = 4 * 1024 * 1024 * 1024L;
+    }
+
+    private void Clear() {
+        foreach (string name in assetBundleNames) {
+            string configKey = string.Format("AssetHash_{0}", name);
+            PlayerPrefs.SetString(configKey, "");
+        }
+        PlayerPrefs.Save();
+        Caching.ClearCache();
+    }
+
+    public virtual void FetchStatus(StatusCallback onFinish, bool clearAssetBundles = true) {
+        if (onFinish != null) {
+            //
+            if (clearAssetBundles) {
+                foreach (var ab in assetBundles)
+                    ab.Destroy();
+                assetBundles.Clear();
+            }
+
+            //
+            onFinish(true, null, 0);
+        }
+    }
+
+    public void FetchServers(Action<bool, string> callback) {
+        //
+        StopAllCoroutines();
+        StartCoroutine(_FetchServers(callback));
+    }
+
+    private IEnumerator _FetchServers(Action<bool, string> callback) {
+        using (var www = new WWW(Config.webURL + "servers.php?sns_id={0}&t={1}".SFormat(MyGameManager.Get().GetSNSID(), UnityEngine.Random.value))) {
+            yield return www;
+
+            try {
+                var json = JSON.Parse(www.text) as JSONClass;
+
+                serverGroups.Clear();
+                foreach (JSONNode node in json["servers"].AsArray) {
+                    var sgi = new ServerGroupInfo(node);
+                    if (sgi.id <= 0) {
+                        // 로컬 서버
+                        // 로컬 테스트 일 경우, Config.fixHost or defaultHost 로 접속한다.
+                        if (Application.isEditor ||
+                            Constants.DEVELOPMENT_MODE) {
+                            sgi.host = Config.fixHost;
+
+                            Debug.Log(">>>>> sgi.host : " + sgi.host + " <- from Config.fixHost");
+                        }
+
+                        if (Application.isEditor ||
+                            Constants.DEVELOPMENT_MODE ||
+                            CurrentServiceType.type == ServiceType.DEV) {
+                            serverGroups.Add(sgi);
+                        }
+                    } else {
+                        // 정식 서버
+                        serverGroups.Add(sgi);
+                    }
+                }
+            } catch (Exception) {
+                Debug.LogWarning(www.error);
+                callback(false, "FetchServers failed. Please check your internet connection..");
+                yield break;
+            }
+        }
+
+        callback(true, null);
+
+    }
+
+    public void LoadAll(Action<bool, string> _onFinish, ProgressCallback onProgress) {
+
+        Action<bool> onFinish = delegate (bool success) {
+
+            if (success == false) {
+                if (_onFinish != null)
+                    _onFinish(false, null);
+                return;
+            }
+
+            //
+            try {
+                Localizer.Get().Initialize();
+                ResourceManager.Reload();
+                BadWordFilter.Init();
+                ValidNameFilter.Init();
+                GameData.Reload();
+                ContinuousPlacement.Get().Reload();
+                StatCalculator.Reload();
+
+                //
+                UnloadCommonAssetBundle();
+            } catch (Exception e) {
+                Debug.LogWarning(e);
+
+                if (_onFinish != null) {
+                    _onFinish(false, e.ToString());
+                }
+                return;
+            }
+
+            if (_onFinish != null)
+                _onFinish(success, null);
+            return;
+        };
+
+        //
+        if (Config.ignoreAssetBundle) {
+            if (onFinish != null)
+                onFinish(true);
+
+            return;
+        }
+
+        //
+        StartCoroutine(
+            Load(assetBundleNames, delegate (bool success) {
+                if (onFinish != null)
+                    onFinish(success);
+            }, onProgress));
+    }
+
+    IEnumerator Load(IEnumerable<string> names, Action<bool> onFinish, ProgressCallback onProgress) {
+
+        yield return null;
+        yield return Resources.UnloadUnusedAssets();
+
+        //
+        foreach (var ab in assetBundles)
+            ab.Destroy();
+        assetBundles.Clear();
+
+        if (onProgress != null)
+            onProgress(0f);
+
+        //
+        bool success = true;
+        int i = 0;
+        var nameCount = names.Count();
+        foreach (var name in names) {
+            //
+            Stream stream = null;
+            AssetBundleCreateRequest req = null;
+
+            // 일단 StreamingAssets 폴더에서 가져오기를 시도한다.
+            try {
+                string path = Path.Combine(Application.streamingAssetsPath, "AssetBundles/" + name + ".unity3d");
+
+                stream = new XorStream(new FileStream(path, FileMode.Open, FileAccess.Read), KEY);
+                req = AssetBundle.LoadFromStreamAsync(stream);
+            } catch {
+            }
+
+            // 로딩 시도!
+            if (req != null)
+                yield return req;
+
+            //
+            if (req != null && req.assetBundle) {
+                // PASS
+            } else {
+
+                try {
+                    if (stream != null)
+                        stream.Close();
+                } catch {
+
+                }
+
+                // persistentDataPath 폴더에서 가져오기를 시도한다.
+                try {
+                    string path = this is AssetBundleManager_NXPatcher ? NXPatcher.GetFilePath(name + ".unity3d")
+                        : Path.Combine(persistentDataPath, name + ".unity3d");
+
+                    stream = new XorStream(new FileStream(path, FileMode.Open, FileAccess.Read), KEY);
+                    req = AssetBundle.LoadFromStreamAsync(stream);
+                } catch {
+                }
+
+                // 로딩 시도!
+                if (req != null)
+                    yield return req;
+            }
+
+            try {
+                if (req != null && req.assetBundle != null) {
+                    req.assetBundle.name = name;
+
+                    //
+                    var abInfo = new AssetBundleInfo(req.assetBundle, stream);
+
+                    //
+                    if (name == "Commons")
+                        commonsAssetBundle = abInfo;
+
+                    //
+                    assetBundles.Add(abInfo);
+                    Debug.Log(string.Format("Loaded {0}.unity3d", name));
+
+                    if (onProgress != null)
+                        onProgress(++i / (float) nameCount);
+                } else {
+                    try {
+                        if (stream != null)
+                            stream.Close();
+                    } catch {
+
+                    }
+
+                    //
+                    success = false;
+
+                    string configKey = string.Format("AssetHash_{0}", name);
+                    PlayerPrefs.SetString(configKey, "");
+                    Debug.LogWarning(string.Format("Failed to load {0}.unity3d", name));
+                    break;
+                }
+            } catch (Exception e) {
+                Debug.LogException(e);
+                success = false;
+
+                //
+                string configKey = string.Format("AssetHash_{0}", name);
+                PlayerPrefs.SetString(configKey, "");
+                Debug.LogWarning(string.Format("Failed to load {0}.unity3d", name));
+                break;
+            }
+        }
+
+        //
+        if (success)
+            loadedAll = true;
+        onFinish(success);
+
+        yield break;
+    }
+
+
+    public virtual void DownloadAll(ProgressCallback onProgress, DownloadCallback onFinish) {
+
+    }
+
+    public UnityEngine.Object GetAsset(string name, Type type) {
+        foreach (var ab in assetBundles) {
+            var obj = ab.assetBundle.LoadAsset(name, type);
+            if (obj)
+                return obj;
+        }
+
+        return null;
+    }
+
+    public void GetAssetAsync(string name, Type type, Action<UnityEngine.Object> callback) {
+        StartCoroutine(GetAssetCoroutine(name, type, callback));
+    }
+
+    public IEnumerator GetAssetCoroutine(string name, Type type, Action<UnityEngine.Object> callback) {
+        var args = name.Split('/');
+        string filterAssetBundle = "Commons";
+        string fileName = name;
+
+        if (args.Length >= 2) {
+            filterAssetBundle = args[0];
+            fileName = args[args.Length - 1];
+        }
+
+        foreach (var ab in assetBundles) {
+            // 애셋번들을 필터링 해야할 경우,
+            if (filterAssetBundle != null && string.Compare(ab.assetBundle.name, filterAssetBundle, true) != 0)
+                continue;
+
+            //
+            var req = ab.assetBundle.LoadAssetAsync(fileName, type);
+            yield return req;
+
+            if (req.asset) {
+                callback(req.asset);
+                yield break;
+            }
+        }
+
+        callback(null);
+    }
+
+}
---
+++ Assets/Script/Core/AssetBundleManager_Custom.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using CreativeSpore.RpgMapEditor;
+using SimpleJSON;
+using Nexon.Pub;
+using UnityEngine.Networking;
+
+public class AssetBundleManager_Custom : AssetBundleManager {
+    public override void Awake() {
+
+
+    }
+
+    private static List<string> GetDownloadableAssetBundleNames() {
+        if (CurrentServiceType.type == ServiceType.DEV)
+            return assetBundleNames;
+
+#if UNITY_IOS && !UNITY_EDITOR
+        return new List<string>() {
+            "Commons",
+        };
+#endif
+
+        return assetBundleNames;
+    }
+
+    public override void FetchStatus(StatusCallback onFinish, bool clearAssetBundles = true) {
+        base.FetchStatus((bool success, string message, long fileSize) => {
+            if (success) {
+                StopAllCoroutines();
+                StartCoroutine(DownloadMeta(GetDownloadableAssetBundleNames(), onFinish));
+            } else {
+                onFinish(false, null, 0);
+            }
+        }, clearAssetBundles);
+    }
+
+    public override void DownloadAll(ProgressCallback onProgress, DownloadCallback onFinish) {
+        //
+        if (Config.ignoreAssetBundle) {
+            if (onFinish != null)
+                onFinish(true, null);
+
+            return;
+        }
+
+        StopAllCoroutines();
+        StartCoroutine(Download(GetDownloadableAssetBundleNames(), onProgress, onFinish));
+    }
+
+    IEnumerator DownloadMeta(IEnumerable<string> names, StatusCallback onFinish) {
+        //
+        if (Config.ignoreAssetBundle) {
+            if (onFinish != null)
+                onFinish(true, null, 0);
+
+            yield break;
+        }
+
+        string postfix = "";
+        string folder = "android/";
+
+        if (Constants.DEVELOPMENT_MODE) {
+#if UNITY_IPHONE
+			folder = "ios_beta";
+#elif UNITY_ANDROID
+			folder = "android_beta";
+#endif
+        } else {
+#if UNITY_IPHONE
+			folder = "ios";
+#elif UNITY_ANDROID
+			folder = "android";
+#endif
+        }
+
+        string url = null;
+        long fileSize = 0;
+
+        foreach (var name in names) {
+            url = CurrentServiceType.updateURL_Prefix + folder + "/" + name + postfix + ".md5?t=" + _version;
+            using (var www = new WWW(url)) {
+                yield return www;
+
+                if (www.error != null) {
+                    Debug.LogWarning(www.error);
+                    fileSize = -1;
+                    www.Dispose();
+                    break;
+                }
+
+                string[] contents = www.text.Split('/');
+                www.Dispose();
+
+                string configKey = string.Format("AssetHash_{0}", name);
+                string crc = contents.GetSafe(2, contents.GetSafe(0));
+                if (PlayerPrefs.GetString(configKey, null) == crc)
+                    continue;
+
+                if (contents.Length >= 2)
+                    fileSize += long.Parse(contents[1]);
+            }
+        }
+
+        // 
+        onFinish(true, null, fileSize);
+    }
+
+
+    IEnumerator Download(IEnumerable<string> names, ProgressCallback onProgress, DownloadCallback onFinish) {
+
+        bool success = true;
+        string postfix = "";
+        string folder = "android";
+
+        if (Constants.DEVELOPMENT_MODE) {
+#if UNITY_IPHONE
+			folder = "ios_beta";
+#elif UNITY_ANDROID
+			folder = "android_beta";
+#endif
+        } else {
+#if UNITY_IPHONE
+			folder = "ios";
+#elif UNITY_ANDROID
+			folder = "android";
+#endif
+        }
+
+        string url = null;
+        long fileSize = 0;
+        int fileIndex = 0;
+
+        foreach (var name in names) {
+            //
+            string configKey = string.Format("AssetHash_{0}", name);
+            string crc;
+
+            //
+            url = CurrentServiceType.updateURL_Prefix + folder + "/" + name + postfix + ".md5?t=" + _version;
+            using (var www = new WWW(url)) {
+                yield return www;
+
+                if (www.error != null) {
+                    Debug.LogWarning(www.error);
+                    success = false;
+                    break;
+                }
+
+                string[] contents = www.text.Split('/');
+                crc = contents.GetSafe(2, contents.GetSafe(0));
+                Debug.Log(string.Format("Patch CRC {0}.unity3d = {1}", name, crc));
+
+                //
+                if (PlayerPrefs.GetString(configKey, null) == crc) {
+                    Debug.Log(string.Format("Skipping {0}.unity3d = {1}", name, PlayerPrefs.GetString(configKey, null)));
+
+                    fileIndex++;
+                    if (onProgress != null)
+                        onProgress(fileIndex / (float) names.Count());
+                    continue;
+                }
+
+                fileSize = long.Parse(contents[1]);
+            }
+
+            //
+            var path = Path.Combine(persistentDataPath, name + ".unity3d");
+            url = CurrentServiceType.updateURL_Prefix + folder + "/" + name + postfix + ".unity3d?t=" + _version;
+            using (var www = new UnityWebRequest(url, UnityWebRequest.kHttpVerbGET)) {
+                www.downloadHandler = new DownloadHandlerFile(path);
+
+                //
+                var op = www.SendWebRequest();
+                while (!op.isDone) {
+                    yield return null;
+
+                    //
+                    if (onProgress != null)
+                        onProgress((fileIndex + Mathf.Clamp01(www.downloadedBytes / (float) fileSize)) / (float) names.Count());
+                }
+
+                if (www.isNetworkError || www.isHttpError) {
+                    Debug.LogWarning(string.Format("Failed to download {0}.unity3d", name));
+                    success = false;
+                    break;
+                }
+            }
+
+            //				if(md5.CompareTo(Utility.MD5(bytes)) != 0) {
+            //					Debug.LogWarning(string.Format("Failed to download {0}.unity3d", name));
+            //					success = false;
+            //					break;
+            //				}
+
+            try {
+#if UNITY_IPHONE
+				UnityEngine.iOS.Device.SetNoBackupFlag(path);
+#endif
+            } catch (Exception e) {
+                Debug.LogException(e);
+                Debug.LogWarning(string.Format("Failed to save {0}.unity3d", name));
+                success = false;
+                break;
+            }
+
+            fileIndex++;
+            if (onProgress != null)
+                onProgress(fileIndex / (float) names.Count());
+            PlayerPrefs.SetString(configKey, crc);
+
+            //
+            Debug.Log(string.Format("Downloaded {0}.unity3d", name));
+        }
+
+        PlayerPrefs.Save();
+        onFinish(success, null);
+    }
+}
