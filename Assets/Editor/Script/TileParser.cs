b799ba6f05463aa9684c9ac89bbce6f399e7667c
HUAWEI\JS <jsfumato@naver.com>
2019-10-06 19:14:29 +0900

    parser 만들자

---
+++ Assets/Editor/Script/TileParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+
+public class TileParser : MonoBehaviour {
+    public Texture2D origin;
+    public Texture2D parsedResult;
+
+    public void OnParseTileImage() {
+        if (origin == null)
+            return;
+
+        //
+        parsedResult = new Texture2D(origin.width, origin.height, TextureFormat.RGBA32, false);
+
+        //
+
+    }
+}
